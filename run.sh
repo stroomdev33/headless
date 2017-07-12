92dd52ac260087ef0013ee3bb25443d6c9eadb6e
stroomdev33 <stroomdev33@gmail.com>
2017-07-12 15:51:30 +0000

    minor update

--- run.sh
+++ run.sh
@@ -4,7 +4,6 @@
 #
 
 export TESTDIR=$HOME
-#export `eval echo ~$USER`
 
 #./stroom.sh stroom.headless.Headless input=$TESTDIR/input/repo output=$TESTDIR/output/RESULT config=$TESTDIR/input/StroomConfig.zip tmp=$TESTDIR/tmp
 ./stroom.sh
